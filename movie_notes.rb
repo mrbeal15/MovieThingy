49fcce881772b4cd45f711d6d95926aa6b438fc0
Lucy Boone <lucy.a.boone@gmail.com>
2015-11-11 15:03:45 -0600

    pseudocode

---
+++ movie_notes.rb
@@ -0,0 +1,9 @@
+# Visitors to the site can browse by genre or search for a specific movie. If they do not have an account, they can read reviews, but cannot comment, or rate a review.
+# To comment and rate reviews, users must create an account and log in.
+# Once logged in, users can comment on reviews, and rate them on a scale of 1-5. Five indicates an outstanding review, and one indicates a review of little substance.
+# When rating a review, users can see the review's current average rating as well as the rating they are assigning.
+# Once a user has made at least three submissions that have an average rating of 4 or higher, with a minimum of three high scores/submission, they are deemed to be a "trusted" user.
+# Trusted user reviews can post without community oversite.
+# Reviews from all other users must be vetted before they can be posted.
+# If a trusted users' overall ratings total falls below 4, they will be reverted back to a standard user.
+# User type can be admin, trusted, or standard.
