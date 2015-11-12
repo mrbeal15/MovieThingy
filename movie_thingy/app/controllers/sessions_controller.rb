e61b619e4b807bc344bb2b76927881e4b404d098
Nicole Tolch <email": null>
2015-11-12 09:55:20 -0600

    Add login page

--- movie_thingy/app/controllers/sessions_controller.rb
+++ movie_thingy/app/controllers/sessions_controller.rb
@@ -1,4 +1,10 @@
 class SessionsController < ApplicationController
+	# get '/login'
+	def new
+		@user = User.new
+	end
+
+	# post '/login'
 	def create
 		@user = User.find_by(email: params[:email])
 		if @user && @user.authenticate()
@@ -6,10 +12,11 @@ class SessionsController < ApplicationController
 			redirect_to "/"
 		else
 			@error = "Invalid credentials"
-			render #...
+			render :new
 		end
 	end
 
+	# delete '/logout'
   def destroy
     session.clear(:user_id)
     redirect_to "/"
