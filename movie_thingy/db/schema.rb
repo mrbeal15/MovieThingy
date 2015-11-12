3f8fc7784cbce7db5b543e49a83b20fbb2de5bb5
Lucy Boone <lucy.a.boone@gmail.com>
2015-11-12 09:31:26 -0600

    add password functionality to user, install bcrypt

--- movie_thingy/db/schema.rb
+++ movie_thingy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151111222900) do
+ActiveRecord::Schema.define(version: 20151112151633) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,12 +71,12 @@ ActiveRecord::Schema.define(version: 20151111222900) do
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "email"
-    t.string   "password"
+    t.string   "password_digest"
     t.text     "bio"
     t.boolean  "admin"
     t.boolean  "trusted_user"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
   add_foreign_key "comments", "reviews"
