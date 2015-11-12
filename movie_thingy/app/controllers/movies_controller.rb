31f51ad8520df9dfe9f50997113c7c6b83f2b615
Ben Fallon <bnfallon@gmail.com>
2015-11-12 10:07:49 -0600

    add movies_controller

---
+++ movie_thingy/app/controllers/movies_controller.rb
@@ -0,0 +1,23 @@
+class PagesController < ApplicationController
+
+  def index
+    @movies = Movie.all
+  end
+
+  def show
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+	def create
+    @movie = Movie.create( movie_params )
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title_photo, :title, :director, :release_year, :runtime, :mppa_rating, :cast, :genre_id)
+  end
+end
