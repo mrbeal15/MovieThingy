8954aa811467771e64b94df1c6359b78e7c8c51f
LucyBoone <lucy.a.boone@gmail.com>
2015-11-13 14:55:39 -0600

    fix typ in review seed

--- movie_thingy/db/seeds.rb
+++ movie_thingy/db/seeds.rb
@@ -27,7 +27,7 @@ Genre.create(name: "Thriller")
 20.times{ Review.create(
       content: Faker::Lorem.paragraph,
       user_id: rand(2..10),
-      movie_id: rand(1..12)
+      movie_id: rand(1..12),
       photography: rand(1..5),
       directing: rand(1..5),
       cast: rand(1..5),
