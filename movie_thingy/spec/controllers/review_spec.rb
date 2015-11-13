e6d54d5d4aba41434e066468dfcb1bc47dfc06c8
Ben Fallon <bnfallon@gmail.com>
2015-11-13 10:58:08 -0600

    add post#create tests for reviews

--- movie_thingy/spec/controllers/review_spec.rb
+++ movie_thingy/spec/controllers/review_spec.rb
@@ -11,4 +11,35 @@ RSpec.describe ReviewsController, type: :controller do
     end
   end
 
+  describe 'GET #show' do
+    it "assigns the requested review as @review" do
+      get :show, { movie_id: 1, id: review.to_param }
+      expect(assigns(:review)).to eq(review)
+    end
+  end
+
+  describe 'GET #new' do
+    it 'renders the new form' do
+      get :new, { :movie_id => 1 }
+      expect(response).to render_template :new
+    end
+  end
+
+  describe 'POST #create' do
+    context "when valid params are passed" do
+
+      it 'creates a new review' do
+        expect{
+          post :create, { movie_id: 1, review: {title: "awesome movie", content: "This movie was super awesome", photography: 5, directing: 5, cast: 5, score: 5, overall_effect:5}
+        }}.to change(Review, :count).by(1)
+      end
+
+      it 'redirects to the movie page after creation' do
+        expect(
+          post :create, { movie_id: 1, review: {title: "awesome movie", content: "This movie was super awesome", photography: 5, directing: 5, cast: 5, score: 5, overall_effect:5}
+        }).to redirect_to("/movies/#{assigns(:movie).id}")
+      end
+    end
+  end
+
 end
