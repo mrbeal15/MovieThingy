3cacb6d6ea3bbd4d9365b5b00e7c93007f96434d
Lucy Boone <lucy.a.boone@gmail.com>
2015-11-12 18:18:56 -0600

    add initial voting logic

--- movie_thingy/app/models/review.rb
+++ movie_thingy/app/models/review.rb
@@ -4,5 +4,18 @@ class Review < ActiveRecord::Base
   has_many :ratings, :as => :rateable
   has_many :comments
 
-  
+  def review_average_rating
+    # user = User.find_by(id: :user_id)
+    # user_reviews = current_user.reviews
+    sum = []
+    user = self.user
+    user.reviews.last.ratings.each do |rate|
+      sum << rate.rating
+    end
+    total = sum.reduce(:+)
+    average = total / sum.length
+    return average
+  end
+
+
 end
