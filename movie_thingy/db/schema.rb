701b9435382d81e776f2db22a613b76858e73b5a
Nicole Tolch <email": null>
2015-11-13 14:31:23 -0600

    Add avg_scores to movies, top movies (and logic) to index

--- movie_thingy/db/schema.rb
+++ movie_thingy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151112203653) do
+ActiveRecord::Schema.define(version: 20151113202122) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20151112203653) do
     t.string   "title_photo_content_type"
     t.integer  "title_photo_file_size"
     t.datetime "title_photo_updated_at"
+    t.integer  "avg_score"
   end
 
   create_table "ratings", force: :cascade do |t|
