2ce6f92f052da05d5f02d4534f972cae40e9123a
LucyBoone <lucy.a.boone@gmail.com>
2015-11-13 14:54:26 -0600

    add additional seed data back into file

--- movie_thingy/db/seeds.rb
+++ movie_thingy/db/seeds.rb
@@ -6,170 +6,199 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-                      Movie.create(
-                        genre_id: 2,
-                        title: "The Wolf of Wall Street",
-                        director: "Martin Scorsese",
-                        release_year: 2013,
-                        runtime: 180,
-                        mppa_rating: "R",
-                        cast: "Leonardo DiCaprio, Jonah Hill, Margot Robbie",
-                        title_photo_file_name: nil,
-                        title_photo_content_type: nil,
-                        title_photo_file_size: nil,
-                        title_photo_updated_at: nil
-                        )
+10.times{ User.create(
+      username: Faker::Name.first_name,
+      email: Faker::Internet.email,
+      password: Faker::Lorem.words(1),
+      bio: Faker::Lorem.paragraph,
+     )}
 
-                        Movie.create(
-                          genre_id: 8,
-                          title: "The Great Gatsby",
-                          director: "Jack Clayton",
-                          release_year: 1974,
-                          runtime: 144,
-                          mppa_rating: "PG",
-                          cast: "Robert Redford, Mia Farrow, Bruce Dern",
-                          title_photo_file_name: nil,
-                          title_photo_content_type: nil,
-                          title_photo_file_size: nil,
-                          title_photo_updated_at: nil
-                          )
+Genre.create(name: "Action")
+Genre.create(name: "Comedy")
+Genre.create(name: "Drama")
+Genre.create(name: "Family")
+Genre.create(name: "Horror")
+Genre.create(name: "Musical")
+Genre.create(name: "Noir")
+Genre.create(name: "Romance")
+Genre.create(name: "Science Fiction")
+Genre.create(name: "Thriller")
 
-                          Movie.create(
-                            genre_id: 3,
-                            title: "The Great Gatsby",
-                            director: "Baz Luhrmann",
-                            release_year: 2013,
-                            runtime: 143,
-                            mppa_rating: "PG-13",
-                            cast: "Leonardo DiCaprio, Carey Mulligan, Joel Edgerton",
-                            title_photo_file_name: nil,
-                            title_photo_content_type: nil,
-                            title_photo_file_size: nil,
-                            title_photo_updated_at: nil
-                            )
+20.times{ Review.create(
+      content: Faker::Lorem.paragraph,
+      user_id: rand(2..10),
+      movie_id: rand(1..12)
+      photography: rand(1..5),
+      directing: rand(1..5),
+      cast: rand(1..5),
+      score: rand(1..5),
+      overall_effect: rand(1..5)
+     )}
 
-                            Movie.create(
-                             genre_id: 3,
-                             title: "The Great Gatsby",
-                             director: "Baz Luhrmann",
-                             release_year: 2013,
-                             runtime: 143,
-                             mppa_rating: "PG-13",
-                             cast: "Leonardo DiCaprio, Carey Mulligan, Joel Edgerton",
-                             title_photo_file_name: nil,
-                             title_photo_content_type: nil,
-                             title_photo_file_size: nil,
-                             title_photo_updated_at: nil
-                             )
+Movie.create(
+  genre_id: 2,
+  title: "The Wolf of Wall Street",
+  director: "Martin Scorsese",
+  release_year: 2013,
+  runtime: 180,
+  mppa_rating: "R",
+  cast: "Leonardo DiCaprio, Jonah Hill, Margot Robbie",
+  title_photo_file_name: nil,
+  title_photo_content_type: nil,
+  title_photo_file_size: nil,
+  title_photo_updated_at: nil
+)
 
-                            Movie.create(
-                              genre_id: 8,
-                              title: "Titanic",
-                              director: "James Cameron",
-                              release_year: 1997,
-                              runtime: 194,
-                              mppa_rating: "PG-13",
-                              cast: "Leonardo DiCaprio, Kate Winslet, Billy Zane",
-                              title_photo_file_name: nil,
-                              title_photo_content_type: nil,
-                              title_photo_file_size: nil,
-                              title_photo_updated_at: nil
-                              )
+Movie.create(
+  genre_id: 8,
+  title: "The Great Gatsby",
+  director: "Jack Clayton",
+  release_year: 1974,
+  runtime: 144,
+  mppa_rating: "PG",
+  cast: "Robert Redford, Mia Farrow, Bruce Dern",
+  title_photo_file_name: nil,
+  title_photo_content_type: nil,
+  title_photo_file_size: nil,
+  title_photo_updated_at: nil
+)
 
-                              Movie.create(
-                                genre_id: 8,
-                                title: "Titanic",
-                                director: "James Cameron",
-                                release_year: 1997,
-                                runtime: 194,
-                                mppa_rating: "PG-13",
-                                cast: "Leonardo DiCaprio, Kate Winslet, Billy Zane",
-                                title_photo_file_name: nil,
-                                title_photo_content_type: nil,
-                                title_photo_file_size: nil,
-                                title_photo_updated_at: nil
-                                )
+Movie.create(
+  genre_id: 3,
+  title: "The Great Gatsby",
+  director: "Baz Luhrmann",
+  release_year: 2013,
+  runtime: 143,
+  mppa_rating: "PG-13",
+  cast: "Leonardo DiCaprio, Carey Mulligan, Joel Edgerton",
+  title_photo_file_name: nil,
+  title_photo_content_type: nil,
+  title_photo_file_size: nil,
+  title_photo_updated_at: nil
+)
 
-                                Movie.create(
-                                  genre_id: 11,
-                                  title: "Shutter Island",
-                                  director: "Martin Scorsese",
-                                  release_year: 2010,
-                                  runtime: 138,
-                                  mppa_rating: "R",
-                                  cast: "Leonardo DiCaprio, Emily Mortimer, Mark Ruffalo",
-                                  title_photo_file_name: nil,
-                                  title_photo_content_type: nil,
-                                  title_photo_file_size: nil,
-                                  title_photo_updated_at: nil
-                                  )
+Movie.create(
+  genre_id: 3,
+  title: "The Great Gatsby",
+  director: "Baz Luhrmann",
+  release_year: 2013,
+  runtime: 143,
+  mppa_rating: "PG-13",
+  cast: "Leonardo DiCaprio, Carey Mulligan, Joel Edgerton",
+  title_photo_file_name: nil,
+  title_photo_content_type: nil,
+  title_photo_file_size: nil,
+  title_photo_updated_at: nil
+)
 
-                                  Movie.create(
-                                    genre_id: 11,
-                                    title: "Shutter Island",
-                                    director: "Martin Scorsese",
-                                    release_year: 2010,
-                                    runtime: 138,
-                                    mppa_rating: "R",
-                                    cast: "Leonardo DiCaprio, Emily Mortimer, Mark Ruffalo",
-                                    title_photo_file_name: nil,
-                                    title_photo_content_type: nil,
-                                    title_photo_file_size: nil,
-                                    title_photo_updated_at: nil
-                                    )
+Movie.create(
+  genre_id: 8,
+  title: "Titanic",
+  director: "James Cameron",
+  release_year: 1997,
+  runtime: 194,
+  mppa_rating: "PG-13",
+  cast: "Leonardo DiCaprio, Kate Winslet, Billy Zane",
+  title_photo_file_name: nil,
+  title_photo_content_type: nil,
+  title_photo_file_size: nil,
+  title_photo_updated_at: nil
+)
 
-                                    Movie.create(
-                                      genre_id: 10,
-                                      title: "Inception",
-                                      director: "Christopher Nolan",
-                                      release_year: 2010,
-                                      runtime: 148,
-                                      mppa_rating: "PG-13",
-                                      cast: "Leonardo DiCaprio, Joseph Gordon-Levitt, Ellen Page",
-                                      title_photo_file_name: nil,
-                                      title_photo_content_type: nil,
-                                      title_photo_file_size: nil,
-                                      title_photo_updated_at: nil
-                                      )
+Movie.create(
+  genre_id: 8,
+  title: "Titanic",
+  director: "James Cameron",
+  release_year: 1997,
+  runtime: 194,
+  mppa_rating: "PG-13",
+  cast: "Leonardo DiCaprio, Kate Winslet, Billy Zane",
+  title_photo_file_name: nil,
+  title_photo_content_type: nil,
+  title_photo_file_size: nil,
+  title_photo_updated_at: nil
+)
 
-                                       Movie.create(
-                                        genre_id: 10,
-                                        title: "Inception",
-                                        director: "Christopher Nolan",
-                                        release_year: 2010,
-                                        runtime: 148,
-                                        mppa_rating: "PG-13",
-                                        cast: "Leonardo DiCaprio, Joseph Gordon-Levitt, Ellen Page",
-                                        title_photo_file_name: nil,
-                                        title_photo_content_type: nil,
-                                        title_photo_file_size: nil,
-                                        title_photo_updated_at: nil
-                                        )
+Movie.create(
+  genre_id: 11,
+  title: "Shutter Island",
+  director: "Martin Scorsese",
+  release_year: 2010,
+  runtime: 138,
+  mppa_rating: "R",
+  cast: "Leonardo DiCaprio, Emily Mortimer, Mark Ruffalo",
+  title_photo_file_name: nil,
+  title_photo_content_type: nil,
+  title_photo_file_size: nil,
+  title_photo_updated_at: nil
+)
 
-                                       Movie.create(
-                                        genre_id: 13,
-                                        title: "Django Unchained",
-                                        director: "Quentin Tarantino",
-                                        release_year: 2012,
-                                        runtime: 165,
-                                        mppa_rating: "R",
-                                        cast: "Jamie Foxx, Christoph Waltz, Leonardo DiCaprio",
-                                        title_photo_file_name: nil,
-                                        title_photo_content_type: nil,
-                                        title_photo_file_size: nil,
-                                        title_photo_updated_at: nil
-                                        )
+Movie.create(
+  genre_id: 11,
+  title: "Shutter Island",
+  director: "Martin Scorsese",
+  release_year: 2010,
+  runtime: 138,
+  mppa_rating: "R",
+  cast: "Leonardo DiCaprio, Emily Mortimer, Mark Ruffalo",
+  title_photo_file_name: nil,
+  title_photo_content_type: nil,
+  title_photo_file_size: nil,
+  title_photo_updated_at: nil
+)
 
-                                        Movie.create(
-                                         genre_id: 1,
-                                         title: "The Man in the Iron Mask",
-                                         director: "Randall Wallace",
-                                         release_year: 1998,
-                                         runtime: 132,
-                                         mppa_rating: "PG-13",
-                                         cast: "Leonardo DiCaprio, Jeremy Irons, John Malkovich",
-                                         title_photo_file_name: nil,
-                                         title_photo_content_type: nil,
-                                         title_photo_file_size: nil,
-                                         title_photo_updated_at: nil
-                                         )
+Movie.create(
+  genre_id: 10,
+  title: "Inception",
+  director: "Christopher Nolan",
+  release_year: 2010,
+  runtime: 148,
+  mppa_rating: "PG-13",
+  cast: "Leonardo DiCaprio, Joseph Gordon-Levitt, Ellen Page",
+  title_photo_file_name: nil,
+  title_photo_content_type: nil,
+  title_photo_file_size: nil,
+  title_photo_updated_at: nil
+)
+
+Movie.create(
+  genre_id: 10,
+  title: "Inception",
+  director: "Christopher Nolan",
+  release_year: 2010,
+  runtime: 148,
+  mppa_rating: "PG-13",
+  cast: "Leonardo DiCaprio, Joseph Gordon-Levitt, Ellen Page",
+  title_photo_file_name: nil,
+  title_photo_content_type: nil,
+  title_photo_file_size: nil,
+  title_photo_updated_at: nil
+)
+
+Movie.create(
+  genre_id: 13,
+  title: "Django Unchained",
+  director: "Quentin Tarantino",
+  release_year: 2012,
+  runtime: 165,
+  mppa_rating: "R",
+  cast: "Jamie Foxx, Christoph Waltz, Leonardo DiCaprio",
+  title_photo_file_name: nil,
+  title_photo_content_type: nil,
+  title_photo_file_size: nil,
+  title_photo_updated_at: nil
+)
+
+Movie.create(
+  genre_id: 1,
+  title: "The Man in the Iron Mask",
+  director: "Randall Wallace",
+  release_year: 1998,
+  runtime: 132,
+  mppa_rating: "PG-13",
+  cast: "Leonardo DiCaprio, Jeremy Irons, John Malkovich",
+  title_photo_file_name: nil,
+  title_photo_content_type: nil,
+  title_photo_file_size: nil,
+  title_photo_updated_at: nil
+)
